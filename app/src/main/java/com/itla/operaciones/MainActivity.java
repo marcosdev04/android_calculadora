77b950add16859baa7a8c8ce8fa5bc229777a3a0
Marcos <marcos.mfd04@gmail.com>
2019-09-28 15:47:07 -0400

    completado, optimizar codigo

--- app/src/main/java/com/itla/operaciones/MainActivity.java
+++ app/src/main/java/com/itla/operaciones/MainActivity.java
@@ -11,11 +11,12 @@ public class MainActivity extends AppCompatActivity {
 
     Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0;
 
-    Button btnRes,btnMul,btnDiv,btnSum,btnResul,btnModu,btnPun,btnEli;
+    Button btnRes,btnMul,btnDiv,btnSum,btnResul,btnPorc,btnPun,btnEli;
 
     TextView txtResult;
 
-    String agregar,operacion;
+    String agregar,operacion,ultioperador;
+    double resumo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         btnDiv = findViewById(R.id.btnDiv);
         btnMul = findViewById(R.id.btnMul);
         btnResul = findViewById(R.id.btnResul);
-        btnModu = findViewById(R.id.btnMod);
+        btnPorc = findViewById(R.id.btnPorc);
         btnPun = findViewById(R.id.btnPun);
         btnEli = findViewById(R.id.btnEli);
 
@@ -153,6 +154,118 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        /* -- OPERACIONES -- */
+
+        btnMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operacion = "multi";
+
+                if(ultioperador!=""){
+                    return;
+                }
+                ultioperador = operacion;
+                resumo += Double.parseDouble(txtResult.getText().toString());
+                txtResult.setText("");
+            }
+        });
+
+
+        btnRes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operacion = "resta";
+                if(ultioperador!=""){
+                    return;
+                }
+                ultioperador = operacion;
+                resumo += Double.parseDouble(txtResult.getText().toString());
+                txtResult.setText("");
+
+
+
+            }
+        });
+
+        btnDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operacion = "divi";
+                if(ultioperador!=""){
+                    return;
+                }
+                ultioperador = operacion;
+                resumo += Double.parseDouble(txtResult.getText().toString());
+                txtResult.setText("");
+            }
+        });
+
+        btnPorc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operacion = "porc";
+                if(ultioperador!=""){
+                    return;
+                }
+                ultioperador = operacion;
+                resumo = Double.parseDouble(txtResult.getText().toString());
+                txtResult.setText("");
+            }
+        });
+
+        btnSum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operacion = "suma";
+                if(ultioperador!=""){
+                    return;
+                }
+                ultioperador = operacion;
+                resumo += Double.parseDouble(txtResult.getText().toString());
+                txtResult.setText("");
+            }
+        });
+
+
+        btnResul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (operacion){
+                    case "multi":
+                        resumo = resumo * (Double.parseDouble(txtResult.getText().toString()));
+                        txtResult.setText(String.valueOf(resumo));
+                        resumo = 0;
+                        break;
+                    case "resta":
+                        resumo = resumo - (Double.parseDouble(txtResult.getText().toString()));
+                        txtResult.setText(String.valueOf(resumo));
+                        resumo = 0;
+                        break;
+                    case "divi":
+                        resumo = resumo / (Double.parseDouble(txtResult.getText().toString()));
+                        txtResult.setText(String.valueOf(resumo));
+                        resumo = 0;
+                        break;
+                    case "porc":
+                        resumo = (Double.parseDouble(txtResult.getText().toString()) * resumo) / 100;
+                        txtResult.setText(String.valueOf(resumo));
+                        resumo = 0;
+                    case "suma":
+                        resumo = Double.parseDouble(txtResult.getText().toString()) + resumo;
+                        txtResult.setText(String.valueOf(resumo));
+                        resumo = 0;
+                }
+                ultioperador = "";
+            }
+
+
+        });
+
+
+
     }
+
+
 
 }
